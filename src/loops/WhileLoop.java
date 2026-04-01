4a66d1ae2c39eeeb9835b948f3eff51e217ee59d
shiva1821 <vishalmudhiraj151@gmail.com>
2026-04-01 12:38:04 +0530

    added

---
+++ src/loops/WhileLoop.java
@@ -0,0 +1,34 @@
+package loops;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+<<<<<<< HEAD
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("");
+		System.out.println("***********");
+		System.out.println("");
+
+
+
+       int i = 1;
+       while (i<=5) {
+    	   System.out.println(i);
+    	   i++;
+       }
+       
+	}
+}
+       
+=======
+		int i =7;
+		while(i<10) {
+			System.out.println("*");
+			i++;
+		}
+	}
+}
+>>>>>>> ccf5dc609a68c2b2bbf5b7781dc22f2e30684f72
